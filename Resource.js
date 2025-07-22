bfd229b1016264a7dfd55a81b6e6f5d4143e6b29
Mark Dierolf <mdierolf@cloudquote.io>
2025-07-21 18:31:26 -0700

    Added constructor to `Resource` class for flexible initialization

--- Resource.js
+++ Resource.js
@@ -5,7 +5,22 @@
  * A resource that provides information.
  */
 export default class Resource {
-	description = "The Subclass should have set this";
+	/** @type {string} The name of the resource */
+	name;
+	/** @type {string} A description of the resource */
+	description;
+
+	/**
+	 * Creates a new Resource instance
+	 * @param {Object} params - The initialization parameters
+	 * @param {string} params.name - The name of the resource
+	 * @param {string} params.description - The description of the resource
+	 */
+	constructor({ name, description } = {}) {
+		this.name ??= name;
+		this.description ??= description;
+	}
+	//description = "The Subclass should have set this";
 
 	/**
 	 * Initialize and start the service
